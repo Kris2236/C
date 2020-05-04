6698b32df55bc4dd0034fcad24c3283a74540ea2
Kris2236 <63661068+Kris2236@users.noreply.github.com>
2020-05-04 09:36:17 +0200

    save Matrix in 2d array

--- 2 - arrays and pointers/tri.c
+++ 2 - arrays and pointers/tri.c
@@ -1,23 +1,61 @@
+/* Napisz program, który na wejściu otrzymuje macierz kwadratową i sprawdza, czy jest
+ona trójkątna dolna. Macierz L jest dolnotrójkątna, jeżeli lij = 0 dla i < j (czyli nad
+główną przekątną znajdują się elementy zerowe). */
+
 #include <stdio.h>
+
+printMatrix(int** M, int n)
+{
+    printf("\n");
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            printf("%d ", M[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+int triangular_upper(int** M, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(M[i][j] != 0 && j > i)
+                return 0;
+        }
+    }
+
+    return 1;
+}
 
 int main()
 {
     int n = 0;
     scanf("%d", &n);
 
-    int success = 1;
+    int** M = malloc(sizeof(int*) * n);
+    for(int i=0; i<n; i++)
+    {
+        M[i] = malloc(sizeof(int) * n);
+    }
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
             int a = 0;
             scanf("%d", &a);
-
-            if (i < j && a != 0)
-                success = 0;
+            M[i][j] = a;
         }
     }
 
-    printf(success ? "YES" : "NO");
+    printMatrix(M, n);
+
+    printf(triangular_upper(M, n) ? "YES" : "NO");
 
     return 0;
 }
