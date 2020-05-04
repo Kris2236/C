6ac62d0784fce41354c77b4e11158367542e8066
Kris2236 <63661068+Kris2236@users.noreply.github.com>
2020-05-04 12:52:49 +0200

    faster algorithm

--- 2 - arrays and pointers/merge.c
+++ 2 - arrays and pointers/merge.c
@@ -1,5 +1,37 @@
+/* Dane s¹ dwie tablice liczb ca³kowitych, T1 i T2, o d³ugoœciach odpowiednio m i n. Napisz
+program, który na wejœciu otrzymuje wartoœci obu tablic i sortuje je rosn¹co. Nastêpnie
+utwórz now¹ tablicê, T3, o d³ugoœci co najwy¿ej m + n, która powstaje przez po³¹czenie tablic T1 i T2 w taki sposób, aby:
+1. Zachowaæ rosn¹ce uporz¹dkowanie wartoœci tablicy
+2. Wartoœci tablicy T3 by³y unikalne    */
+
 #include <stdio.h>
 #include <stdlib.h>
+
+void unique(int* T1, int* T2, int* T3, int n, int m)
+{
+
+    int* tmp = malloc(sizeof(int) * (n+m));
+    for(int i=0; i< n; i++)
+        tmp[i] = T1[i];
+    for(int i=n; i< (n+m); i++)
+        tmp[i] = T2[i-n];
+
+    bubble_sort(tmp, (m+n));
+    printTab(tmp, m+n);
+
+    int id=1;
+    T3[0] = tmp[0];
+
+    for(int i=1; i<(m+n); i++)
+    {
+        if(tmp[i-1] != tmp[i])
+        {
+            T3[id] = tmp[i];
+            id++;
+        }
+    }
+    printTab(T3, id);
+}
 
 void swap(int* a, int* b)
 {
@@ -8,109 +40,51 @@ void swap(int* a, int* b)
     *b = c;
 }
 
-void bubble_sort(int tab[], int N)
-{
-    for (int i = N - 1; i > 0; --i)
-    {
+void bubble_sort(int* tab, int N){
+    for (int i = N - 1; i > 0; --i){
         int sorted = 1;
-
-        for (int j = 0; j < i; ++j)
-        {
-            if (tab[j] > tab[j + 1])
-            {
+        for (int j = 0; j < i; ++j){
+            if (tab[j] > tab[j + 1]){
                 swap(tab + j, tab + j + 1);
                 sorted = 0;
             }
         }
-
         if (sorted)
             return;
     }
 }
 
-int* merge(int* T1, int m, int* T2, int n, int* T3)
+void read(int* tab, int size)
 {
-    if (m == 0 && n == 0)
-        return 0;
-
-    int len = 0;
-    int p1 = 0;
-    int p2 = 0;
-
-    // Assigning the first element
-    if (m != 0 && n != 0)
+    for(int i=0; i<size; i++)
     {
-        if (T1[0] < T2[0])
-            T3[len++] = T1[p1++];
-        else
-            T3[len++] = T2[p2++];
+        int input = 0;
+        scanf("%d", &input);
+        tab[i] = input;
     }
+}
 
-    while (p1 < m && p2 < n)
-    {
-        if (T1[p1] < T2[p2])
-        {
-            if (T3[len - 1] != T1[p1])
-                T3[len++] = T1[p1];
-            p1++;
-        }
-        else
-        {
-            if (T3[len - 1] != T2[p2])
-                T3[len++] = T2[p2];
-            p2++;
-        }
-    }
-
-    // Still something left at the end of T1
-    while (p1 < m)
-    {
-        if (T3[len - 1] != T1[p1])
-            T3[len++] = T1[p1];
-        p1++;
-    }
-
-
-    // Still something left at the end of T2
-    while (p2 < n)
-    {
-        if (T3[len - 1] != T2[p2])
-            T3[len++] = T2[p2];
-        p2++;
-    }
-
-    return len;
+void printTab(int* tab, int n)
+{
+    for(int i=0; i<n; i++)
+        printf("%d ", tab[i]);
+    printf("\n");
 }
 
 int main()
 {
-    int m = 0;
-    int n = 0;
+    int m=0, n=0;
+    scanf("%d %d", &n, &m);
 
-    scanf("%d %d", &m, &n);
+    int T1 = malloc(sizeof(int) * n);
+    int T2 = malloc(sizeof(int) * m);
 
-    int* T1 = malloc(sizeof(int) * m);
-    int* T2 = malloc(sizeof(int) * n);
+    read(T1, n);
+    read(T2, m);
 
-    for (int i = 0; i < m; ++i)
-        scanf("%d", T1 + i);
+    int T3 = malloc(sizeof(int) * (n+m));
 
-    for (int i = 0; i < n; ++i)
-        scanf("%d", T2 + i);
+    unique(T1, T2, T3, n, m);
 
-    bubble_sort(T1, m);
-    bubble_sort(T2, n);
-
-    int* T3 = malloc(sizeof(int) * (m + n));
-    int t3 = merge(T1, m, T2, n, T3);
-
-    free(T1);
-    free(T2);
-
-    for (int i = 0; i < t3; ++i)
-        printf("%d\n", T3[i]);
-
-    free(T3);
-
-    return 0;
+   return 0;
 }
